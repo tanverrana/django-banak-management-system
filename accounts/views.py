4de1f0054e9e82c464c3babcf2213682298f6073
pro <pro@prodeMacBook-Pro-2.local>
2023-09-18 23:00:18 +0600

    send data to database

--- accounts/views.py
+++ accounts/views.py
@@ -9,9 +9,10 @@ from django.urls import reverse_lazy
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
     form_class = UserRegistrationForm
-    success_url = reverse_lazy('reverse')
+    success_url = reverse_lazy('register')
 
     def form_valid(self, form):
+        print(form.cleaned_data)
         user = form.save()
-        login(user)
+        login(self.request, user)
         return super().form_valid(form)
