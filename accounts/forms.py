942cdf1bdaa4e0e4624c2b20ca574ce00913967e
pro <pro@prodeMacBook-Pro-2.local>
2023-09-18 22:18:14 +0600

    show ui

--- accounts/forms.py
+++ accounts/forms.py
@@ -46,3 +46,15 @@ class UserRegistrationForm(UserCreationForm):
                     account_no=10000 + our_user.id
                 )
             return our_user
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field in self.fields:
+                self.fields[field].widget.attrs.update({
+                    'class': (
+                        'appearance-none block w-full bg-gray-200'
+                        'text-gray-700 border border-gray-200 rounded'
+                        'py-3 px-4 leading-tight focus:outline-none'
+                        'focus:bg-white focus:border-gray-500'
+                    )
+                })
