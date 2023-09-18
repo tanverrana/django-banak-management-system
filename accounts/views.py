ec4af343260321e8ee06311c06c39072f8cbb1d2
pro <pro@prodeMacBook-Pro-2.local>
2023-09-18 21:22:12 +0600

    user regestration

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import UserRegistrationForm
 from django.contrib.auth import login
+from django.urls import reverse_lazy
 # Create your views here.
 
 
-class UserRegistration(FormView):
-    template_name = ''
+class UserRegistrationView(FormView):
+    template_name = 'accounts/user_registration.html'
     form_class = UserRegistrationForm
-    success_url = ''
+    success_url = reverse_lazy('reverse')
 
     def form_valid(self, form):
         user = form.save()
