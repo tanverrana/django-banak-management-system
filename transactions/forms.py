fd66479802f6487782df577edf1d4c4d92c58279
pro <pro@prodeMacBook-Pro-2.local>
2023-09-24 09:58:28 +0600

    deposit amount

--- transactions/forms.py
+++ transactions/forms.py
@@ -17,3 +17,15 @@ class TransactionForm(forms.ModelForm):
         self.instance.account = self.account
         self.instance.balance_after_transaction = self.account.balance
         return super().save()
+
+
+class DepositForm(TransactionForm):
+    def clean_amount(self):  # amount field ke filter korbo
+        min_deposit_amount = 100
+        # user er fillup kor a form theke amount field er value ke niye ashlam
+        amount = self.cleaned_data.get('amount')
+        if amount < min_deposit_amount:
+            raise forms.ValidationError(
+                f'you need to deposit at least {min_deposit_amount} $'
+            )
+        return amount
