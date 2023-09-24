24ed75f50377e91a934dcc3f90cba89118b5f455
pro <pro@prodeMacBook-Pro-2.local>
2023-09-25 00:38:21 +0600

    transaciotns admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+
+# from transactions.models import Transaction
 from .models import Transaction
-# Register your models here.
-admin.site.register(Transaction)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['account', 'amount', 'balance_after_transaction',
+                    'transaction_type', 'loan_approve']
+
+    def save_model(self, request, obj, form, change):
+        obj.account.balance += obj.amount
+        obj.balance_after_transaction = obj.account.balance
+        obj.account.save()
+        super().save_model(request, obj, form, change)
